e3dbce7c87501eb365f892c27a2124fbe3db4c7f
Katherine Shakman <katherineshakman@hh10neurosc-001.cpmc.columbia.edu>
2017-03-03 13:50:54 -0500

    Updated coloredBoxPlot

--- coloredBoxPlot.m
+++ coloredBoxPlot.m
@@ -1,7 +1,7 @@
 % coloredBoxPlot by Katie Shakman - 2/23/2017
 
 function coloredBoxPlot(data, labelNums, labels, ...
-    edgeColors, boxStyle, fillColor, opacity, indicesToFill,yRange)
+    edgeColors, boxStyle, fillColor, opacity, indicesToFill)
 
 % choose option 1 or 2 for plotting
 option = 1; 
@@ -9,20 +9,19 @@ addScatter = 'on';
 
 if option == 1
     boxplot(data, labels, ...
-        'Widths', 0.3, 'Colors', edgeColors, 'Boxstyle', boxStyle)
+        'Widths', 0.2, 'Colors', edgeColors, 'Boxstyle', boxStyle)
     % Inspired by
     % www.mathworks.com/matlabcentral/newsreader/view_thread/300245:
     h = findobj(gca,'Tag','Box');
     for j=indicesToFill
         patch(get(h(j),'XData'),get(h(j),'YData'),fillColor,'FaceAlpha',opacity);
     end
-    ylim(yRange);
     box off;
     if strcmp(addScatter,'on')
         hold on 
         for idx = min(labelNums):max(labelNums)
-            scatterG = data(labelNums==idx)
-            position = ones(size(scatterG))*idx
+            scatterG = data(labelNums==idx);
+            position = ones(size(scatterG))*idx;
             scatter(position, scatterG); 
         end
     end
